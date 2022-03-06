b096f025eeee8e51307ed92a54d343bbc29ced54
nikhilgupta58 <nikhil@tartanhq.com>
2022-03-06 17:39:37 +0530

    final

--- components/ErrorToast.tsx
+++ components/ErrorToast.tsx
@@ -2,26 +2,22 @@ import { createStandaloneToast, theme } from '@chakra-ui/react';
 import React from 'react'
 
 const toast = createStandaloneToast({ theme });
-type Error = { message: string };
+type Error = {
+    message: string,
+    extensions: {
+        code: string
+    },
+    description: string
+};
 export function queryErrorHandler(error: Error | any): void {
     // error is type unknown because in js, anything can be an error (e.g. throw(5))
     const id = "react-query-error";
-    let title: string = "";
-
-    if (error?.message) {
-        title = error.message;
-    } else {
-        title =
-            error instanceof Error
-                ? // remove the initial 'Error: ' that accompanies many errors
-                error.toString().replace(/^Error:\s*/, "")
-                : "Error connecting to server";
-    }
+    const err = error?.description ? error?.description : error?.extensions?.code
     // prevent duplicate toasts
     toast.closeAll();
     toast({
         id,
-        title,
+        title: err,
         status: "error",
         variant: "subtle",
         isClosable: true,
