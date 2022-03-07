0e8524c04b027ca0b73a6ef5b480f1172004d999
nikhilgupta58 <nikhil@tartanhq.com>
2022-03-07 10:51:56 +0530

    Add image

--- components/Feed/component/PostBox/PostBox.container.tsx
+++ components/Feed/component/PostBox/PostBox.container.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useReactiveVar } from '@apollo/client'
+import { Axios } from 'axios'
 import { useRouter } from 'next/router'
 import React from 'react'
 import { userInfoVar } from '../../../../Files/cache'
@@ -16,17 +17,23 @@ export default function PostBoxContainer() {
         imageRef.current.click()
     }
     const [selected, setSelected] = React.useState(null)
+
+    async function fetchData(formData) {
+        const rawResponse = await fetch('https://api.cloudinary.com/v1_1/dhiu02sfh/image/upload', {
+            method: 'POST',
+            body: formData
+        });
+        const content = await rawResponse.json();
+        setSelected(content.secure_url);
+    }
+
     const uploadImage = async () => {
-        try {
-            const file = imageRef.current.files[0]
-            const reader = new FileReader();
-            const url = reader.readAsDataURL(file)
-            reader.onloadend = function (e) {
-                setSelected(reader.result)
-            }
-        } catch (e) {
-            // console.log(e)
-        }
+        const axios = new Axios()
+        const files = imageRef.current.files
+        const formData = new FormData()
+        formData.append('file', files[0])
+        formData.append("upload_preset", "zpzdwhik")
+        fetchData(formData)
     }
     const [body, setBody] = React.useState('')
     const setHeading = (e) => {
